2a2d4264c492a55c1aa1e606bd3a289aa298e508
cwgreene <archgoon@gmail.com>
2023-10-16 18:25:31 -0700

    Add in type identification with words.

--- scanner.py
+++ scanner.py
@@ -1,5 +1,6 @@
 import time
 from SnifferAPI import Sniffer
+import SnifferAPI.Types as Types
 
 sniffer = Sniffer.Sniffer("COM4")
 sniffer.start()
@@ -9,11 +10,14 @@ def hexAddr(blist):
 
 while True:
     for packet in sniffer.getPackets():
+        types = {Types.PACKET_TYPE_UNKNOWN:"UNKNOWN",
+                 Types.PACKET_TYPE_ADVERTISING: "ADVERTISING",
+                 Types.PACKET_TYPE_DATA: "DATA"}
         ble = packet.blePacket
         print(packet.time, packet.RSSI, end=" ")
         if ble:
             dest = hexAddr(ble.scanAddress) if ble.scanAddress else "Broadcast"
-            print("type:", ble.advType, "src:", hexAddr(ble.advAddress), "dest:", dest, end=" ")
+            print("type:", types[ble.type], "src:", hexAddr(ble.advAddress), "dest:", dest, end=" ")
             if ble.name != "\"\"":
                 print("name:", ble.name)
             print("")
