d3ab06f83fcd7950b4afb326d952a4e05d05f033
cwgreene <archgoon@gmail.com>
2023-10-17 09:06:56 -0700

    Refactor to have argparse.

--- scanner.py
+++ scanner.py
@@ -1,10 +1,30 @@
+import argparse
+
 import time
 from SnifferAPI import Sniffer
 import SnifferAPI.Types as Types
 import platform
 #def list_silicon_devices():
 
-def main():
+
+def hexAddr(blist):
+    return ":".join([hex(i)[2:] for i in blist])
+
+def packet_to_string(packet):
+        res = ""
+        types = {Types.PACKET_TYPE_UNKNOWN:"UNKNOWN",
+                    Types.PACKET_TYPE_ADVERTISING: "ADVERTISING",
+                    Types.PACKET_TYPE_DATA: "DATA"}
+        ble = packet.blePacket
+        res = " ".join([res, str(packet.time), str(packet.RSSI)])
+        if ble:
+            dest = hexAddr(ble.scanAddress) if ble.scanAddress else "Broadcast"
+            res = " ".join([res, "type:", types[ble.type], "src:", hexAddr(ble.advAddress), "dest:", dest])
+            if ble.name[1:-1] != "":
+                res += f" name: '{ble.name[1:-1]}'"
+        return res
+
+def get_default_device():
     # TODO: Make this autodectable and manually configurable
     # just not hard coded for crying out loud.
     if platform.system() == "Windows":
@@ -17,27 +37,21 @@ def main():
         device_name = "/dev/ttyUSB0"
     else:
         raise("I have no idea what OS this is:", platform.system())
-    sniffer = Sniffer.Sniffer(device_name)
-    sniffer.start()
+    return device_name
 
-    def hexAddr(blist):
-        return ":".join([hex(i)[2:] for i in blist])
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--device", help="Device name (/dev/ttyUSB0 or COM4)", default=get_default_device())
+    parser.add_argument("--buckets", action="store_true")
+    
+    options = parser.parse_args()
+
+    sniffer = Sniffer.Sniffer(options.device)
+    sniffer.start()
 
     while True:
         for packet in sniffer.getPackets():
-            types = {Types.PACKET_TYPE_UNKNOWN:"UNKNOWN",
-                     Types.PACKET_TYPE_ADVERTISING: "ADVERTISING",
-                     Types.PACKET_TYPE_DATA: "DATA"}
-            ble = packet.blePacket
-            print(packet.time, packet.RSSI, end=" ")
-            if ble:
-                dest = hexAddr(ble.scanAddress) if ble.scanAddress else "Broadcast"
-                print("type:", types[ble.type], "src:", hexAddr(ble.advAddress), "dest:", dest, end=" ")
-                if ble.name[1:-1] != "\"\"":
-                    print(f"name: \"{ble.name[1:-1]}\"", end=" ")
-                print("")
-            else:
-                print("")
-            #print(dir(packet))
-        time.sleep(.1)
+            print(packet_to_string(packet))
+        time.sleep(.1) # really? is this *really* the best way?
 main()
