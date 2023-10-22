2b1f62c8d39a3f2a1b160cf639a7dadb9a84e617
cwgreene <archgoon@gmail.com>
2023-10-21 21:44:19 -0700

    Add in --json

--- scanner.py
+++ scanner.py
@@ -2,10 +2,29 @@ import argparse
 
 import time
 from SnifferAPI import Sniffer
+from SnifferAPI.Packet import Packet as APIPacket
 import SnifferAPI.Types as Types
 import platform
 import math
+import json
 #def list_silicon_devices():
+
+TYPES = {
+    Types.PACKET_TYPE_UNKNOWN:"UNKNOWN",
+    Types.PACKET_TYPE_ADVERTISING: "ADVERTISING",
+    Types.PACKET_TYPE_DATA: "DATA"
+}
+
+ADV_TYPES = {
+    Types.ADV_TYPE_ADV_IND : "ADV_IND",
+    Types.ADV_TYPE_ADV_DIRECT_IND   : "ADVV_DIRECT_IND",
+    Types.ADV_TYPE_ADV_NONCONN_IND  : "ADV_NONCONN_IND",
+    Types.ADV_TYPE_ADV_SCAN_IND     : "ADV_SCAN_IND",
+    Types.ADV_TYPE_SCAN_REQ         : "SCAN_REQ",
+    Types.ADV_TYPE_SCAN_RSP         : "SCAN_RSP",
+    Types.ADV_TYPE_CONNECT_REQ      : "CONNECT_REQ",
+    Types.ADV_TYPE_ADV_EXT_IND      : "ADV_EXT_IND"
+}
 
 class Bucket:
     def __init__(self):
@@ -16,21 +35,44 @@ class Bucket:
 def hexAddr(blist):
     return ":".join([hex(i)[2:] for i in blist])
 
-def packet_to_string(packet):
+def packet_to_string(packet : APIPacket):
         res = ""
-        types = {Types.PACKET_TYPE_UNKNOWN:"UNKNOWN",
-                    Types.PACKET_TYPE_ADVERTISING: "ADVERTISING",
-                    Types.PACKET_TYPE_DATA: "DATA"}
         ble = packet.blePacket
         res = " ".join([res, str(packet.time), str(packet.RSSI)])
         if ble:
             dest = hexAddr(ble.scanAddress) if ble.scanAddress else "Broadcast"
-            res = " ".join([res, "type:", types[ble.type], "src:", hexAddr(ble.advAddress), "dest:", dest])
+            res = " ".join([res, "type:", TYPES[ble.type], "src:", hexAddr(ble.advAddress), "dest:", dest])
             if ble.name[1:-1] != "":
                 res += f" name: '{ble.name[1:-1]}'"
         return res
 
-def update_buckets(packet, buckets):
+class Packet:
+    def __init__(self, packet : APIPacket):
+        self.packet = packet
+    
+    def to_json(self):
+        packet = self.packet
+        ble = self.packet.blePacket
+        if not ble:
+            return {"rssi": self.packet.RSSI}
+        js = {
+            "timestamp": packet.timestamp,
+            "rssi": packet.RSSI,
+            "src": hexAddr(ble.advAddress),
+            "type": TYPES[ble.type],
+        }
+        if ble.type == Types.PACKET_TYPE_ADVERTISING:
+            js["advType"] = ADV_TYPES[ble.advType]
+        js["dest"] = hexAddr(ble.scanAddress) if ble.scanAddress else "Broadcast"
+        if ble.name[1:-1] != "":
+            js["name"] = ble.name[1:-1]
+        return js
+
+    def __str__(self):
+        return packet_to_string(self.packet)
+
+
+def update_buckets(packet : APIPacket, buckets : dict):
     ble = packet.blePacket
     if not ble:
         return
@@ -45,7 +87,7 @@ def update_buckets(packet, buckets):
     # TODO: make average decay with time
     bucket.avg_rssi = (((bucket.avg_rssi*(bucket.count-1)) + packet.RSSI)/bucket.count)
 
-def display_buckets(buckets):
+def display_buckets(buckets : dict):
     #print("===")
     length = min(len(buckets), 10)
     for addr in reversed(sorted(buckets, key= lambda b: buckets[b].count)[-10:]):
@@ -74,6 +116,7 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--device", help="Device name (/dev/ttyUSB0 or COM4)", default=get_default_device())
+    parser.add_argument("--json", action="store_true")
     parser.add_argument("--buckets", action="store_true")
     
     options = parser.parse_args()
@@ -85,11 +128,15 @@ def main():
     try:
         while True:
             for packet in sniffer.getPackets():
+                packet = Packet(packet)
                 if options.buckets:
-                    update_buckets(packet, buckets)
+                    update_buckets(packet.packet, buckets)
                     display_buckets(buckets)
                 else:
-                    print(packet_to_string(packet))
+                    if options.json:
+                        print(packet.to_json())
+                    else:
+                        print(packet)
             time.sleep(.1) # really? is this *really* the best way?
     except Exception as e:
         print("\n"*min(len(buckets),10))
